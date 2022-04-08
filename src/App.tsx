014fb6e5955d8ba3d2ee175766e29d1daa54594c
FengYungz <lucasfeng83@gmail.com>
2022-04-08 16:29:10 -0300

    :white_check_mark: adding toast alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../src/assets/styles/global.css';
 import Page from "./page/inicio/index";
-//import { ToastContainer, toast } from 'react-toastify';
-//import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <>
     <Page />
+    <ToastContainer />
     </>
   );
 }
